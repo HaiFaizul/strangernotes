faab33cb62421cac318144c7678b970bfb3eace5
Faizul Hai <140169441+HaiFaizul@users.noreply.github.com>
2025-05-11 01:16:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/strangernotes/',
   server: {
     host: "::",
     port: 8080,
